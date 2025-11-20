989be57a62fa5f2d62bac40d9ec351689c7289f9
Aleksandr <sassshok228@yandex.ru>
2025-11-20 14:38:08 +0300

    Update basic_analysis.sql

--- basic_analysis.sql
+++ basic_analysis.sql
@@ -2,7 +2,7 @@ SELECT
     COUNT(DISTINCT order_id) as total_order,
     COUNT(DISTINCT customer_id) as unique_customer,
     SUM(total_amount) as total_revenue,
-    AVG(total_amount) as averege_order,
+    AVG(total_amount) as average_order,
     MAX(total_amount) as max_order
 FROM orders 
 WHERE status = 'completed';
